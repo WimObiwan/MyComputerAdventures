69c36b2af52ac1959e87529635cae8d16134671a
root <root@server1.foxinnovations.be>
2018-08-13 09:07:00 +0000

    Fixed rebuild to be quiet to eliminate mails.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #jekyll build --source ./bitwiser/ --destination ./_site/ #--incremental
 #jekyll build --source ./startbootstrap/ --destination ./_site/ #--incremental
-jekyll build --source ./startbootstrap/ --destination ./_site/
+jekyll build --source ./startbootstrap/ --destination ./_site/ -q
