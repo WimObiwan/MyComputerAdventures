3b928d37d3acb5a31e7adab47af5178d83438c25
Wim Devos <wim@obiwan.be>
2018-09-24 00:16:00 +0200

    Rebuild: Fixed jekyll binary path.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #jekyll build --source ./bitwiser/ --destination ./_site/ #--incremental
 #jekyll build --source ./startbootstrap/ --destination ./_site/ #--incremental
-jekyll build --source ./startbootstrap/ --destination ./_site/ -q
+/usr/local/rvm/gems/ruby-2.3.0/bin/jekyll build --source ./startbootstrap/ --destination ./_site/ -q
