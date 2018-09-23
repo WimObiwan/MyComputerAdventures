31f03bcd7ee660180a67b4a9e623216357803c0e
Wim Devos <wim@obiwan.be>
2018-09-24 00:17:00 +0200

    Rebuild: Fixed issues with cron.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #jekyll build --source ./bitwiser/ --destination ./_site/ #--incremental
 #jekyll build --source ./startbootstrap/ --destination ./_site/ #--incremental
-/usr/local/rvm/gems/ruby-2.3.0/bin/jekyll build --source ./startbootstrap/ --destination ./_site/ -q
+
+# Set PATH, to allow running from cron
+PATH=/usr/local/rvm/gems/ruby-2.3.0/bin:/usr/local/rvm/gems/ruby-2.3.0@global/bin:/usr/local/rvm/rubies/ruby-2.3.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/local/rvm/bin
+
+jekyll build --source ./startbootstrap/ --destination ./_site/ -q
